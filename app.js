e89d95282efbab2ad6965d07b847d97a46203384
dravicha <32406238+rdhanushikka@users.noreply.github.com>
2019-04-13 21:15:45 -0400

    Mongoose and other db stuff

--- app.js
+++ app.js
@@ -1,24 +1,117 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 const app = express();
 
+const mongoose = require('mongoose');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dravicha:cs252@boiler-wallet-3r0z7.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("Wallet").collection("clubs");
-  // perform actions on the collection object
-  console.log('connected');
-  client.close();
-});
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://dravicha:cs252@boiler-wallet-3r0z7.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true
+})
+.then(()=> console.log('Mongo connected'))
+.catch(err => console.log(err));
+
+
+require('./models/User');
+const User = mongoose.model('Users');
 
 // telling the system we want to use handlebars template engine
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://dravicha:cs252@boiler-wallet-3r0z7.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// var con, db, server;
+// var connected = false;
+
+
+// const startMongo = async function(){
+//     try {
+//         con = await client.connect();
+//         db = con.db("Wallet");
+//         const collection = db.collection("Users");
+//         console.log('connected to Database Wallet');
+//         connected = true;
+//     } catch (error) {
+//         console.log(error);
+//         return -1;
+//     }
+//     return 0;
+// }
+
+
+// async function stopMongo(){
+//     try{
+//         if(typeof con !== 'undefined'){
+//             con.close();
+//             console.log("stopped Mongo")
+//         }
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// process.on('SIGINT', shutdown);
+
+// // Do graceful shutdown
+// function shutdown() {
+//   console.log('graceful');
+//   stopMongo();
+//   server.close();
+// }
+
+// client.connect(err => {
+//   const collection = client.db("Wallet").collection("Users");
+//   // perform actions on the collection object
+//   console.log('connected');
+// //   const ins= {
+// //     name: 'Sam',
+// //     password: 'scoe',
+// //     codes: ['AXMYCZS', 'WISP123']
+// //   };
+// //   collection.insertOne(ins);
+//     client.close();
+// });
+
+//startMongo();
+
+// (async () => {
+//     await startMongo();
+//     console.log(db);
+// })()
+
+
+
+// setTimeout(function() {
+//     // prints out "2", meaning that the callback is not called immediately after 500 milliseconds.
+//     console.log("Ran after " + (new Date().getSeconds() - s) + " seconds");
+//   }, 500);
+
+
+// const collection = db.collection('Users');
+// console.log(typeof collection);
+// console.log("here");
+// const ins= {
+//     name: '',
+//     password: 'scoe',
+//     codes: ['AXMYCZS', 'WISP123']
+// };
+// collection.insertOne(ins);
+
+
+
+
+
 
 app.use(function(req,res,next){
     console.log(Date.now());
@@ -40,8 +133,13 @@ app.get('/contact', (req,res) => {
     res.render('CONTACT');
 });
 
+app.post('/login', (req,res) =>{
+    console.log(req.body);
+    res.send('ok');
+});
+
 const port = 5061;
 
-app.listen(port, () => {
+server = app.listen(port, () => {
     console.log(`Server started: ${port}`);
 });
