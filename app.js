5cb0f8c00b7814d34c31155258fcb9662e9e02db
“dravicha” <dravicha@purdue.edu>
2019-04-14 11:33:15 -0400

    implemented login

--- app.js
+++ app.js
@@ -27,86 +27,6 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://dravicha:cs252@boiler-wallet-3r0z7.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// var con, db, server;
-// var connected = false;
-
-
-// const startMongo = async function(){
-//     try {
-//         con = await client.connect();
-//         db = con.db("Wallet");
-//         const collection = db.collection("Users");
-//         console.log('connected to Database Wallet');
-//         connected = true;
-//     } catch (error) {
-//         console.log(error);
-//         return -1;
-//     }
-//     return 0;
-// }
-
-
-// async function stopMongo(){
-//     try{
-//         if(typeof con !== 'undefined'){
-//             con.close();
-//             console.log("stopped Mongo")
-//         }
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
-// }
-
-// process.on('SIGINT', shutdown);
-
-// // Do graceful shutdown
-// function shutdown() {
-//   console.log('graceful');
-//   stopMongo();
-//   server.close();
-// }
-
-// client.connect(err => {
-//   const collection = client.db("Wallet").collection("Users");
-//   // perform actions on the collection object
-//   console.log('connected');
-// //   const ins= {
-// //     name: 'Sam',
-// //     password: 'scoe',
-// //     codes: ['AXMYCZS', 'WISP123']
-// //   };
-// //   collection.insertOne(ins);
-//     client.close();
-// });
-
-//startMongo();
-
-// (async () => {
-//     await startMongo();
-//     console.log(db);
-// })()
-
-
-
-// setTimeout(function() {
-//     // prints out "2", meaning that the callback is not called immediately after 500 milliseconds.
-//     console.log("Ran after " + (new Date().getSeconds() - s) + " seconds");
-//   }, 500);
-
-
-// const collection = db.collection('Users');
-// console.log(typeof collection);
-// console.log("here");
-// const ins= {
-//     name: '',
-//     password: 'scoe',
-//     codes: ['AXMYCZS', 'WISP123']
-// };
-// collection.insertOne(ins);
 
 app.use(function(req,res,next){
     console.log(Date.now());
@@ -129,12 +49,7 @@ app.get('/contact', (req,res) => {
 });
 
 app.post('/sign-in-submit', (req, res) => {
-    // dbConnection.then(function(db){
-    //     delete req.body._id;
-    //     db.collection('Users').insertOne(req.body);
-    //     //var value = db.collection('Users').count();
-    //     //console.log(value);
-    // });
+
     let errors = [];
 
     if(!req.body.email){
@@ -148,12 +63,38 @@ app.post('/sign-in-submit', (req, res) => {
         res.render('login', {
             errors:errors,
             email: req.body.email,
-            password: req.body.password
+            password: req.body.password,
+            
         });
     }
     else{
-        console.log(req.body);
-        res.send('User data received now: \n' + JSON.stringify(req.body.inputEmail));
+        const pass = req.body.password;
+        User.findOne({email: req.body.email}, function (err, myUser) {
+            if (myUser != null){
+                if(pass === myUser.password){
+                    res.redirect('clubs');
+                }
+                else{
+                    errors.push({text: 'Wrong password!'});
+                    
+                    res.render('login', {
+                        errors:errors,
+                        email: req.body.email,
+                        password: req.body.password,
+                    });
+                
+                }
+            } 
+            else{
+                errors.push({text: 'Wrong login credentials!'});
+                res.render('login', {
+                    errors:errors,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+            }
+        })
+        
     }
 });
 
